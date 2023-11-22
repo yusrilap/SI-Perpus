b3e467e357c9cf3d73e24b22802ee903cee5ecfe
yusrilap <yusrilazharpermana@gmail.com>
2023-11-22 21:51:46 +0700

    Bab 5 Spatie Laravel Permission

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -41,6 +41,8 @@ class RegisteredUserController extends Controller
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
+        
+        $user->assignRole('Anggota');
 
         event(new Registered($user));
 
--- routes/web.php
+++ routes/web.php
@@ -22,10 +22,35 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::group(['middleware' => ['role:admin']], function () {
+    //
+});
+
+Route::group(['middleware' => ['permission:edit users']], function () {
+    //
+});
+
+// Multiple Roles
+Route::group(['middleware' => ['role:admin|user']], function () {
+    //
+});
+
+// Multiple Permissions
+Route::group(['middleware' => ['permission:edit users|edit articles']], function () {
+    //
+});
+
+Route::group(['middleware' => ['role_or_permission:admin|edit users']], function () {
+    //
+});
+
 
 require __DIR__.'/auth.php';
